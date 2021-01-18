291d448b9489b406032ffc60608ffa460ad08ab1
Arie <arnoudcommandeur@hotmail.com>
2021-01-18 17:19:49 +0100

    Decryption aangepast UI en werkwijze

--- src/js/decrypt.js
+++ src/js/decrypt.js
@@ -7,7 +7,11 @@ App = {
     //alert('App.init');
 
     App.profileStore = initStore();
-    await App.decrypt();
+    const btnDecrypt = document.querySelector('#btnDecrypt');
+
+    btnDecrypt.addEventListener('click', async function(event){
+      await App.decrypt();
+    });
 
     return true;
   },
@@ -23,7 +27,13 @@ App = {
     myPassword = window.prompt("Geef het wachtwoord om de persoonlijke encryptiekey te openen","");
     plainBytes = CryptoJS.AES.decrypt(secretKeyCipher, myPassword)
     secretKey = plainBytes.toString(CryptoJS.enc.Utf8);
-    App.myKey = nacl.box.keyPair.fromSecretKey(nacl.util.decodeBase64(secretKey))
+
+    try {
+      App.myKey = nacl.box.keyPair.fromSecretKey(nacl.util.decodeBase64(secretKey))
+    } catch (error) {
+      alert('Onjuist wachtwoord. Probeer opnieuw.');
+      return false;
+    }
     publicKeySender = nacl.util.decodeBase64(decodeURIComponent(urlParams.get('publicKeySender')));  
 
     nonceD = nacl.util.decodeBase64(decodeURIComponent(urlParams.get('nonceD')));  
@@ -37,75 +47,30 @@ App = {
     document.getElementById("divDescription").innerHTML = nacl.util.encodeUTF8(description);
     document.getElementById("divSecret").innerHTML = nacl.util.encodeUTF8(secret);
 
+    const divStep1 = document.querySelector('#divStep1')
+    const divStep2 = document.querySelector('#divStep2')
+
+    divStep1.style.display = 'none'
+    divStep2.style.display = '';
+
+    divDescription.addEventListener('click', function(event){
+      App.copyToClipboard('#divDescription');
+    });
+    divSecret.addEventListener('click', function(event){
+      App.copyToClipboard('#divSecret');
+    });
+
+
   },
 
-//   decrypt_old: function() {
+  copyToClipboard: function (element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
 
-//     console.log(App.myKey.secretKey);
-//     const queryString = window.location.search;
-//     const urlParams = new URLSearchParams(queryString);
-
-//     omschrijving = decodeURIComponent(urlParams.get('omschrijving'));
-
-
-//     nonce = nacl.util.decodeBase64(decodeURIComponent(urlParams.get('nonce')));  
-//     publicKeySender = nacl.util.decodeBase64(decodeURIComponent(urlParams.get('publicKeySender')));  
-
-//     cipher = nacl.util.decodeBase64(decodeURIComponent(urlParams.get('cipher')));
-
-//     plaintext = nacl.box.open(cipher, nonce, publicKeySender, App.myKey.secretKey);
-// //return;
-//     document.getElementById("omschrijving").innerHTML = omschrijving;
-//     document.getElementById("wachtwoord").innerHTML = nacl.util.encodeUTF8(plaintext);
-
-//     return true;
-//   },
-
-//   initKey: function() { 
-//     return new Promise(function(resolve) {
-//       var r = window.indexedDB.open('VeiligWachtwoordSturen')
-//       r.onupgradeneeded = function() {
-//         var idb = r.result
-//         var store = idb.createObjectStore('Profile', {keyPath: "key"})
-//       }
-//       r.onsuccess = function() {
-//         var idb = r.result
-
-//         let tactn = idb.transaction('Profile', "readonly")
-//         let osc = tactn.objectStore('Profile').openCursor()
-//         osc.onsuccess = function(e) {
-//           let cursor = e.target.result
-//           if (cursor) {
-
-//             cipher = cursor.value["secretkey"];
-//             console.log(cipher);
-//             myWachtwoord = window.prompt("Geef het wachtwoord om de encryptiekey te openen","");
-//             plainBytes = CryptoJS.AES.decrypt(cipher, myWachtwoord)
-//             plain = plainBytes.toString(CryptoJS.enc.Utf8);
-//             console.log(plain);
-
-// //            App.myKey = nacl.box.keyPair.fromSecretKey(nacl.util.decodeBase64(cursor.value["secretkey"])) // Vervangen door nieuwe versie
-//             App.myKey = nacl.box.keyPair.fromSecretKey(nacl.util.decodeBase64(plain))
-//             //console.log(nacl.util.decodeBase64(cursor.value["secretkey"]))
-//             console.log(App.myKey.secretKey)
-
-
-
-//             //storage.innerHTML += "Naam " + cursor.value["volledigeNaam"] + " : voornaam: " +  + ", emailadres " + cursor.value["emailAdres"] + ", publicKey " + cursor.value["publicKey"] + "<br>"
-//             cursor.continue()
-//           }
-//           App.decrypt()
-//         } 
-//         tactn.oncomplete = function() {
-//           idb.close();
-//         }
-
-//       }
-//       r.onerror = function (e) {
-//       alert("Enable to access IndexedDB, " + e.target.errorCode)
-//       }    
-//     })
-//   }
   // end App
 };
 
